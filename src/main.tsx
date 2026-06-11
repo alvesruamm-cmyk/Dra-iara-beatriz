724ee165718a3c8a8cb099786417482e152ff798
alvesruamm-cmyk <alvesruamm@gmail.com>
2026-06-11 00:03:28 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-document.title = "Dra. Iara Beatriz | Implantodontia Avançada";
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
